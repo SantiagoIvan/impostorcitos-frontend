c1406b89cbd880ab8b47c0bc16f4224ea7a92e3d
Santiago Feijoo <santiago.feijoo96@gmail.com>
2025-12-10 17:17:59 -0300

    se muestra correstamente la palabra secreta

---
+++ shared/types/topic.type.ts
@@ -0,0 +1,11 @@
+export enum Topic {
+    ANIME = "Anime",
+    MOVIES = "Peliculas",
+    HOME_APPLIANCES = "Electrodomesticos",
+    GAMES = "Juegos",
+    MUSIC = "Musica",
+    ANIME_CHARACTERS = "Personajes de anime",
+    CLOTHES = "Ropa",
+    SPORTS = "Deportes",
+    DEFAULT = "Anime"
+}
