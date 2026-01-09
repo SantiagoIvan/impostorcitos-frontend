d2c986625c28dc010e1f659f5b96a40cf1b9376f
Santiago Feijoo <santiago.feijoo96@gmail.com>
2026-01-09 16:25:34 -0300

    asd

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -1,10 +1,8 @@
 import {api} from "@/app/services/api";
-const WS_URL = process.env.NEXT_PUBLIC_BACKEND_WS;
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_API;
+
 
 export const AuthService = {
     login: async (username: string) => {
-        console.log("login", WS_URL, API_URL)
         return await api.post("/auth/login", {username});
     },
     logout: async (username: string) => {
