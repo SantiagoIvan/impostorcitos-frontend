0b070d178f0f0bf0c0b9fbd833f3a42bda68cfc2
Santiago Feijoo <santiago.feijoo96@gmail.com>
2026-01-10 20:28:38 -0300

    actualizado objeto env

--- app/config/env.ts
+++ app/config/env.ts
@@ -6,11 +6,11 @@ const required = (value: string | undefined, name: string): string => {
 };
 
 export const ENV = {
-    API_URL: "http://localhost:4000/api",
-    WS_URL: "http://localhost:4000",
-    MIN_PLAYERS_QTY: 3,
-    MAX_MESSAGE_LENGTH: 50,
-    CLEANUP_JOB_INTERVAL: 20000,
-    MESSAGE_TTL: 50000,
+    API_URL: process.env.NEXT_PUBLIC_BACKEND_API,
+    WS_URL: process.env.NEXT_PUBLIC_BACKEND_WS,
+    MIN_PLAYERS_QTY: process.env.NEXT_PUBLIC_MIN_PLAYERS_QTY,
+    MAX_MESSAGE_LENGTH: process.env.MAX_MESSAGE_LENGTH,
+    CLEANUP_JOB_INTERVAL: process.env.CLEANUP_JOB_INTERVAL,
+    MESSAGE_TTL: process.env.MESSAGE_TTL,
 } as const;
 
