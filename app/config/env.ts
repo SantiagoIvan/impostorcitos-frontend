5ebf2529137e7317b3464a4f398aa1f225d389c7
Santiago Feijoo <santiago.feijoo96@gmail.com>
2026-01-12 11:47:40 -0300

    le agregue https al env

--- app/config/env.ts
+++ app/config/env.ts
@@ -4,10 +4,20 @@ const required = (value: string | undefined, name: string): string => {
     }
     return value;
 };
+/*
+API_URL: "https://api.impostorcitos.com/api",
+WS_URL: "https://api.impostorcitos.com",
+
+API_URL: process.env.NEXT_PUBLIC_BACKEND_API,
+WS_URL: process.env.NEXT_PUBLIC_BACKEND_WS,
+
+API_URL: "http://localhost:4000/api",
+WS_URL: "http://localhost:4000",
+ */
 
 export const ENV = {
-    API_URL: "http://api.impostorcitos.com/api",//process.env.NEXT_PUBLIC_BACKEND_API,
-    WS_URL: "http://api.impostorcitos.com", //process.env.NEXT_PUBLIC_BACKEND_WS,
+    API_URL: "https://api.impostorcitos.com/api",//process.env.NEXT_PUBLIC_BACKEND_API,
+    WS_URL: "https://api.impostorcitos.com", //process.env.NEXT_PUBLIC_BACKEND_WS,
     MIN_PLAYERS_QTY: process.env.NEXT_PUBLIC_MIN_PLAYERS_QTY,
     MAX_MESSAGE_LENGTH: process.env.MAX_MESSAGE_LENGTH,
     CLEANUP_JOB_INTERVAL: process.env.CLEANUP_JOB_INTERVAL,
