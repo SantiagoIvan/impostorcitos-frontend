f478a3ffc1976235b57b8d23aa0f280a40082506
Santiago Feijoo <santiago.feijoo96@gmail.com>
2026-01-12 00:59:25 -0300

    ptm no me anda el env

--- app/config/env.ts
+++ app/config/env.ts
@@ -6,8 +6,8 @@ const required = (value: string | undefined, name: string): string => {
 };
 
 export const ENV = {
-    API_URL: process.env.NEXT_PUBLIC_BACKEND_API, // "http://api.impostorcitos.com/api"
-    WS_URL: process.env.NEXT_PUBLIC_BACKEND_WS, // "http://api.impostorcitos.com",
+    API_URL: "http://api.impostorcitos.com/api",//process.env.NEXT_PUBLIC_BACKEND_API,
+    WS_URL: "http://api.impostorcitos.com", //process.env.NEXT_PUBLIC_BACKEND_WS,
     MIN_PLAYERS_QTY: process.env.NEXT_PUBLIC_MIN_PLAYERS_QTY,
     MAX_MESSAGE_LENGTH: process.env.MAX_MESSAGE_LENGTH,
     CLEANUP_JOB_INTERVAL: process.env.CLEANUP_JOB_INTERVAL,
